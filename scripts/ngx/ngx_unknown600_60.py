9eac12c0d92f8a3445055cef3ad265bd29defcb7
arlab <arlab@uf.edu>
2025-01-25 12:17:58 +0000

    repo updated for analysis a-01-N-76

---
+++ scripts/ngx/ngx_unknown600_60.py
@@ -0,0 +1,39 @@
+#!Measurement
+'''
+default_fits: nominal
+'''
+
+def main():
+    info('measurement script')
+
+
+    activate_detectors('H5','H3','H1','L2','L4')
+
+    position_magnet('Ar40', detector='H5')
+
+    
+    eqt = 30
+
+    equilibrate(eqtime=eqt, inlet='MSI', outlet='PIV', delay=3)
+
+    set_time_zero()
+
+    sniff(eqt)
+    set_fits()
+    set_baseline_fits()
+
+    #multicollect on active detectors
+    multicollect(ncounts=600, integration_time=1)
+
+    baselines(ncounts=60, 
+                  mass=37.5, 
+                  detector='AX',
+                  use_dac=True,
+                  settling_time=10)
+    
+    
+    activate_detectors('H5', **{'peak_center':True})
+
+    peak_center(detector='H5',isotope='Ar40', integration_time=1)
+
+    
