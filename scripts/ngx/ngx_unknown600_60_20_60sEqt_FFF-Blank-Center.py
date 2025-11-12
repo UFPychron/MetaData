7f1853c35cc4bbadba60ff4a9eaa9a7113e0b2b2
arlab <arlab@uf.edu>
2025-11-12 18:39:45 +0000

    repo updated for analysis bu-SC1-N-01

---
+++ scripts/ngx/ngx_unknown600_60_20_60sEqt_FFF-Blank-Center.py
@@ -0,0 +1,33 @@
+#!Measurement
+'''
+default_fits: nominal
+'''
+
+def main():
+    info('measurement script')
+
+    activate_detectors('H4','H2','Ax','L3','L5')
+
+    position_magnet('Ar40', detector='H4')
+
+    equilibrate(eqtime=60, inlet='MSI', outlet='PIV', delay=3)
+
+    set_time_zero()
+
+    sniff(30)
+    set_fits()
+    set_baseline_fits()
+
+    sleep(15)
+    #multicollect on active detectors
+    multicollect(ncounts=30, integration_time=20)
+
+    baselines(ncounts=60, 
+                  mass=37.5, 
+                  detector='AX',
+                  use_dac=True,
+                  settling_time=10)
+    
+    activate_detectors('H4', **{'peak_center':True})
+
+    
